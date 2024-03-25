de452860911e4fc7baba040e8609861157635225
karimbaggari <karimbaggari8@gmail.com>
2024-03-25 14:19:03 +0000

    update : few updates in the structure

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as useNavigation } from './src/components/useNavigation';
+export { default as useForm } from './src/components/useForm';
+export { default as useModal } from './src/components/useModal';
+export { default as useDataFetching } from './src/components/useDataFetching';
+export { default as useStateManagement } from './src/components/useStateManagement';
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as useNavigation } from './components/useNavigation';
-export { default as useForm } from './components/useForm';
-export { default as useModal } from './components/useModal';
-export { default as useDataFetching } from './components/useDataFetching';
-export { default as useStateManagement } from './components/useStateManagement';
